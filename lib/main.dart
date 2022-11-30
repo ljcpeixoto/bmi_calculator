d107a10479ae2432057101b7f774f2ac69b6c8ba
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-11-30 07:52:33 -0300

    Copy dark Theme changing only primary and secondary colors

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: const MaterialColor(0xFF0A0E21, {
-          50: Color.fromRGBO(0x0A, 0x0E, 0x21, .1),
-          100: Color.fromRGBO(0x0A, 0x0E, 0x21, .2),
-          200: Color.fromRGBO(0x0A, 0x0E, 0x21, .3),
-          300: Color.fromRGBO(0x0A, 0x0E, 0x21, .4),
-          400: Color.fromRGBO(0x0A, 0x0E, 0x21, .5),
-          500: Color.fromRGBO(0x0A, 0x0E, 0x21, .6),
-          600: Color.fromRGBO(0x0A, 0x0E, 0x21, .7),
-          700: Color.fromRGBO(0x0A, 0x0E, 0x21, .8),
-          800: Color.fromRGBO(0x0A, 0x0E, 0x21, .9),
-          900: Color.fromRGBO(0x0A, 0x0E, 0x21, 1),
-        }),
-        scaffoldBackgroundColor: const Color(0xFF0A0E21),
-        textTheme: const TextTheme(
-          bodyText1: TextStyle(
-            color: Colors.white,
-          ),
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF0A0E21),
+          secondary: Colors.purple,
         ),
       ),
       home: const InputPage(),
