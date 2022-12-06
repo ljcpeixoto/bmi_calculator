1edab47cc54db4fd275903c6c384eb86f4831672
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-12-06 08:11:41 -0300

    Implement initial layout for InputPage

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,22 +16,77 @@ class _InputPageState extends State<InputPage> {
           child: Text('BMI Calculator'),
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Body text',
-        ),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData.dark().copyWith(
-          colorScheme: const ColorScheme.dark().copyWith(
-            secondary: Colors.purple,
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        child: const FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.add),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(15.0),
+              decoration: const BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
