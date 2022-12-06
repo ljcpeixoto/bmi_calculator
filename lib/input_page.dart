dfb84f5a838937a5a14a3f8e43d2049c4cb820b6
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-12-06 19:42:50 -0300

    Create IconContent Widget and add it to the 2 first cards, with male and female icons/texts.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const bottomContainerColor = Color(0xFFEB1555);
@@ -24,12 +25,24 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(
             child: Row(
-              children: const [
+              children: [
                 Expanded(
-                  child: ReusableCard(colour: activeCardColor),
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      iconText: 'MALE',
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: activeCardColor),
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      iconText: 'FEMALE',
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -57,12 +70,48 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String iconText;
+
+  IconContent({
+    required this.icon,
+    required this.iconText,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        const SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          iconText,
+          style: const TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   final Color colour;
+  final Widget? cardChild;
 
   const ReusableCard({
     super.key,
     required this.colour,
+    this.cardChild,
   });
 
   @override
@@ -75,6 +124,7 @@ class ReusableCard extends StatelessWidget {
           Radius.circular(10.0),
         ),
       ),
+      child: cardChild,
     );
   }
 }
